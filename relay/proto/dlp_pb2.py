f6d5220944709f7728e58f397a958328d5d28ab8
h1yeon <108858002+hayeon7898@users.noreply.github.com>
2026-09-05 06:03:05 +0900

    [FIX] X-Corp-Role -> X-Corp-User-Role로 헤더 이름 수정 (#11)
    fix: X-Corp-Role -> X-Corp-User-Role로 헤더 이름 수정 (#10)
    
    - dlp-server의 role_resolver.py가 X-Corp-User-Role을 찾는데 Relay는 Corp-Role로 보내고 있어서 role이 항상 None으로 해석되던 문제
    - 관통 테스트로 발견 (policy.engine 로그에서 role=None 계속 재현됨)

--- relay/proto/dlp_pb2.py
+++ relay/proto/dlp_pb2.py
@@ -4,41 +4,42 @@
 # source: dlp.proto
 # Protobuf Python Version: 7.35.1
 """Generated protocol buffer code."""
-
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
-
 _runtime_version.ValidateProtobufRuntimeVersion(
-    _runtime_version.Domain.PUBLIC, 7, 35, 1, "", "dlp.proto"
+    _runtime_version.Domain.PUBLIC,
+    7,
+    35,
+    1,
+    '',
+    'dlp.proto'
 )
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
-    b'\n\tdlp.proto\x12\x03\x64lp"\xc6\x01\n\x0eInspectRequest\x12\x12\n\nsession_id\x18\x01 \x01(\t\x12\x11\n\tdirection\x18\x02 \x01(\t\x12\x0e\n\x06method\x18\x03 \x01(\t\x12\x0c\n\x04path\x18\x04 \x01(\t\x12\x31\n\x07headers\x18\x05 \x03(\x0b\x32 .dlp.InspectRequest.HeadersEntry\x12\x0c\n\x04\x62ody\x18\x06 \x01(\x0c\x1a.\n\x0cHeadersEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01"C\n\x07Verdict\x12\x0e\n\x06\x61\x63tion\x18\x01 \x01(\t\x12\x18\n\x10transformed_body\x18\x02 \x01(\x0c\x12\x0e\n\x06reason\x18\x03 \x01(\t2<\n\x0c\x44LPInspector\x12,\n\x07Inspect\x12\x13.dlp.InspectRequest\x1a\x0c.dlp.VerdictB=Z;github.com/GenAI-DLP/dlp-proxy-server/internal/dlpclient/pbb\x06proto3'
-)
+
+
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\tdlp.proto\x12\x03\x64lp\"\xc6\x01\n\x0eInspectRequest\x12\x12\n\nsession_id\x18\x01 \x01(\t\x12\x11\n\tdirection\x18\x02 \x01(\t\x12\x0e\n\x06method\x18\x03 \x01(\t\x12\x0c\n\x04path\x18\x04 \x01(\t\x12\x31\n\x07headers\x18\x05 \x03(\x0b\x32 .dlp.InspectRequest.HeadersEntry\x12\x0c\n\x04\x62ody\x18\x06 \x01(\x0c\x1a.\n\x0cHeadersEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"C\n\x07Verdict\x12\x0e\n\x06\x61\x63tion\x18\x01 \x01(\t\x12\x18\n\x10transformed_body\x18\x02 \x01(\x0c\x12\x0e\n\x06reason\x18\x03 \x01(\t2<\n\x0c\x44LPInspector\x12,\n\x07Inspect\x12\x13.dlp.InspectRequest\x1a\x0c.dlp.VerdictB=Z;github.com/GenAI-DLP/dlp-proxy-server/internal/dlpclient/pbb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, "dlp_pb2", _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'dlp_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
-    _globals["DESCRIPTOR"]._loaded_options = None
-    _globals["DESCRIPTOR"]._serialized_options = (
-        b"Z;github.com/GenAI-DLP/dlp-proxy-server/internal/dlpclient/pb"
-    )
-    _globals["_INSPECTREQUEST_HEADERSENTRY"]._loaded_options = None
-    _globals["_INSPECTREQUEST_HEADERSENTRY"]._serialized_options = b"8\001"
-    _globals["_INSPECTREQUEST"]._serialized_start = 19
-    _globals["_INSPECTREQUEST"]._serialized_end = 217
-    _globals["_INSPECTREQUEST_HEADERSENTRY"]._serialized_start = 171
-    _globals["_INSPECTREQUEST_HEADERSENTRY"]._serialized_end = 217
-    _globals["_VERDICT"]._serialized_start = 219
-    _globals["_VERDICT"]._serialized_end = 286
-    _globals["_DLPINSPECTOR"]._serialized_start = 288
-    _globals["_DLPINSPECTOR"]._serialized_end = 348
+  _globals['DESCRIPTOR']._loaded_options = None
+  _globals['DESCRIPTOR']._serialized_options = b'Z;github.com/GenAI-DLP/dlp-proxy-server/internal/dlpclient/pb'
+  _globals['_INSPECTREQUEST_HEADERSENTRY']._loaded_options = None
+  _globals['_INSPECTREQUEST_HEADERSENTRY']._serialized_options = b'8\001'
+  _globals['_INSPECTREQUEST']._serialized_start=19
+  _globals['_INSPECTREQUEST']._serialized_end=217
+  _globals['_INSPECTREQUEST_HEADERSENTRY']._serialized_start=171
+  _globals['_INSPECTREQUEST_HEADERSENTRY']._serialized_end=217
+  _globals['_VERDICT']._serialized_start=219
+  _globals['_VERDICT']._serialized_end=286
+  _globals['_DLPINSPECTOR']._serialized_start=288
+  _globals['_DLPINSPECTOR']._serialized_end=348
 # @@protoc_insertion_point(module_scope)
